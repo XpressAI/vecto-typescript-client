7f6bd691c58fa8fdf7aa1ca95ad95c5b9ad54012
MFA-X-AI <fahreza@xpress.ai>
2023-10-02 17:33:39 +0800

    add generated openapi

---
+++ apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './IndexApi';
+export * from './LookupApi';
+export * from './ManagementApi';
+export * from './UpdateApi';
---
+++ models/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ActionStatusResponse';
+export * from './AnalogyDTO';
+export * from './DataEntry';
+export * from './DataPage';
+export * from './DeleteRequest';
+export * from './DynamicAnalogyLookupRequest';
+export * from './IndexRequest';
+export * from './IndexResponse';
+export * from './JsonTextLookupRequest';
+export * from './LookupRequest';
+export * from './LookupResponse';
+export * from './LookupResult';
+export * from './MetadataUpdateRequest';
+export * from './Modality';
+export * from './ModelInformation';
+export * from './NewTokenRequest';
+export * from './NewVectorSpaceRequest';
+export * from './TokenDTO';
+export * from './TokenResponse';
+export * from './TokenType';
+export * from './UpdateVectorSpaceRequest';
+export * from './UserDTO';
+export * from './VectorSpaceResponse';
+export * from './VectorUpdateRequest';
